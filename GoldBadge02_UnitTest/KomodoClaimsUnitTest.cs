e24e660b3c4df47648d76e323057c5f354e95a5b
blair909 <61510475+blair909@users.noreply.github.com>
2020-04-10 18:51:19 -0500

    Add files via upload

---
+++ GoldBadge02_UnitTest/KomodoClaimsUnitTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using GoldBadge02_ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GoldBadge02_UnitTest
+{
+    [TestClass]
+    public class KomodoClaimsUnitTest
+    {
+        [TestMethod]
+        public void AddContentToDirectory_ShouldGetCorrectBool()
+        {
+            KomodoClaimsContent testContent = new KomodoClaimsContent();
+            KomodoClaimsRepo repo = new KomodoClaimsRepo();
+
+            bool addResult = repo.AddClaimToDirectory(testContent);
+            Assert.IsTrue(addResult);
+        }
+        [TestMethod]
+        public void GetAllDirectory_ShouldReturnCorrectList()
+        {
+            KomodoClaimsContent testContent = new KomodoClaimsContent();
+            KomodoClaimsRepo repo = new KomodoClaimsRepo();
+            repo.AddClaimToDirectory(testContent);
+
+            Queue<KomodoClaimsContent> testList = repo.GetAllClaims();
+            bool directoryHasContent = testList.Contains(testContent);
+            Assert.IsTrue(directoryHasContent);
+        }
+        [TestMethod]
+        public void LookAtNextClaim_ShouldReturnCorrectItem()
+        {
+            KomodoClaimsRepo repo = new KomodoClaimsRepo();
+            KomodoClaimsContent testItem = new KomodoClaimsContent();
+            Queue<KomodoClaimsContent> testQueue = repo.LookAtNextClaim();
+            testQueue.Clear();
+            repo.AddClaimToDirectory(testItem);
+            testQueue.Enqueue(testItem);
+
+            testQueue.Peek();
+            bool directoryHasItem = testQueue.Contains(testItem);
+            Assert.IsTrue(directoryHasItem);
+        }
+        [TestMethod]
+        public void ProcessNextClaim_ShouldReturnCorrectDequeuedItem()
+        {
+            KomodoClaimsRepo repo = new KomodoClaimsRepo();
+            KomodoClaimsContent testItem = new KomodoClaimsContent();
+            Queue<KomodoClaimsContent> testQueue = new Queue<KomodoClaimsContent>();
+            testQueue.Clear();
+            testQueue.Enqueue(testItem);
+            repo.ProcessNextClaim();
+
+            testQueue.Dequeue();
+            bool directoryHasItem = testQueue.Contains(testItem);
+            Assert.IsFalse(directoryHasItem);
+        }
+    }
+}
---
+++ GoldBadge03_UnitTest/KomodoInsuranceUnitTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using GoldBadge03_ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GoldBadge03_UnitTest
+{
+    [TestClass]
+    public class KomodoInsuranceUnitTest
+    {
+        [TestMethod]
+        public void AddContentToDirectory_ShouldGetCorrectBool()
+        {
+            KomodoInsuranceContent content = new KomodoInsuranceContent();
+            KomodoInsuranceRepo repo = new KomodoInsuranceRepo();
+
+            bool addResult = repo.CreateNewBadge(content);
+            Assert.IsTrue(addResult);
+        }
+        [TestMethod]
+        public void GetAllDirectory_ShouldReturnCorrectList()
+        {
+            KomodoInsuranceContent testContent = new KomodoInsuranceContent();
+            KomodoInsuranceRepo repo = new KomodoInsuranceRepo();
+            repo.CreateNewBadge(testContent);
+
+            List<KomodoInsuranceContent> testList = repo.GetAllKeysAndValues();
+            bool directoryHasContent = testList.Contains(testContent);
+            Assert.IsTrue(directoryHasContent);
+        }
+        public KomodoInsuranceContent _content;
+        public KomodoInsuranceRepo _kirepo;
+        [TestInitialize]
+        public void Arrange()
+        {
+            _kirepo = new KomodoInsuranceRepo();
+            _content = new KomodoInsuranceContent(9, "Derp");
+            _kirepo.CreateNewBadge(_content);
+        }
+        [TestMethod]
+        public void DeleteExistingContent_ShouldReturnTrue()
+        {
+            KomodoInsuranceContent toBeDeleted = _kirepo.GetContentByBadgeID(9);
+
+            bool removeResult = _kirepo.DeleteExistingBadgeValue(toBeDeleted);
+            Assert.IsTrue(removeResult);
+        }
+        [TestMethod]
+        public void UpdateExistingContent_ShouldReturnTrue()
+        {
+            KomodoInsuranceContent newContent = new KomodoInsuranceContent(27, "Derpo");
+            bool updateResult = _kirepo.AddOrUpdateDictionaryEntry(9, newContent);
+            Assert.IsTrue(updateResult);
+        }
+    }
+}
